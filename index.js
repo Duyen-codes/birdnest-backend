968486d6581dbe1cacfb8ab2b9df7b90490ff810
Duyen-codes <hongduyen0705@gmail.com>
2022-12-08 13:49:43 +0200

    created a route for fetching a single pilot info

--- index.js
+++ index.js
@@ -43,6 +43,36 @@ app.get("/api/drones", (request, response) => {
 		});
 });
 
+app.get("/api/pilots/:serialNumber", (request, response) => {
+	console.log("request", request);
+	console.log("request.params", request.params.serialNumber);
+	const serialNumber = request.params.serialNumber;
+	console.log("serialNumber", serialNumber);
+
+	fetch(`http://assignments.reaktor.com/birdnest/pilots/${serialNumber}`)
+		.then((response) => {
+			console.log("response", response);
+			console.log("response.body", response.body);
+			return response.text();
+		})
+		.then((data) => {
+			if (data) {
+				// xml2js.parseString(data, (err, result) => {
+				// 	console.log("data", data);
+				// 	console.log("result", result);
+				// 	console.log("err", err);
+
+				// 	const json = JSON.stringify(result, null, 4);
+				// 	console.log("json", json);
+				// 	response.json(json);
+				// });
+				console.log("data", data);
+
+				response.json(data);
+			}
+		});
+});
+
 const unknownEndpoint = (request, response) => {
 	response.status(404).send({ error: "unknown endpoint" });
 };
