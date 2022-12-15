c8f2693bd590dd11f8189df565a66714540e899b
Duyen-codes <hongduyen0705@gmail.com>
2022-12-15 12:48:41 +0200

    streamline deployment, formatted objects retturned by Mongoose by modifying the toJSON method of the schema

--- models/capture.js
+++ models/capture.js
@@ -12,4 +12,11 @@ const captureSchema = mongoose.Schema({
 	],
 });
 
+captureSchema.set("toJSON", {
+	transform: (document, returnedObject) => {
+		returnedObject._id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	},
+});
 module.exports = mongoose.model("capture", captureSchema);
